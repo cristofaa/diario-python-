e681e9265d48e8be58b81177bb92b71fb166634d
cristofaa <47372202+cristofaa@users.noreply.github.com>
2020-10-10 12:03:28 -0500

    Update modulo_kivy.py

--- modulo_kivy.py
+++ modulo_kivy.py
@@ -1,20 +1,183 @@
 import kivy
 from as interger
 
-adfad
-fds
-fa
-sdf
-asdfa
-df
-adf
-asdfa
-sdf
-as
-dfa
-df
-adf
-ad
-fa
-dfa
-sd
+ kivy 
+ kivy.animation 
+ kivy.app 
+ kivy.atlas 
+ kivy.base 
+ kivy.cache 
+ kivy.clock 
+ kivy.compat 
+ kivy.config 
+ kivy.context 
+ kivy.core 
+ kivy.core.audio 
+ kivy.core.camera 
+ kivy.core.clipboard 
+ kivy.core.gl 
+ kivy.core.image 
+ kivy.core.spelling 
+ kivy.core.text 
+ kivy.core.text.markup 
+ kivy.core.text.text_layout 
+ kivy.core.video 
+ kivy.core.window 
+ kivy.deps 
+ kivy.effects 
+ kivy.effects.dampedscroll 
+ kivy.effects.kinetic 
+ kivy.effects.opacityscroll 
+ kivy.effects.scroll 
+ kivy.event 
+ kivy.factory 
+ kivy.garden 
+ kivy.geometry 
+ kivy.gesture 
+ kivy.graphics 
+ kivy.graphics.cgl 
+ kivy.graphics.compiler 
+ kivy.graphics.context 
+ kivy.graphics.context_instructions 
+ kivy.graphics.fbo 
+ kivy.graphics.gl_instructions 
+ kivy.graphics.instructions 
+ kivy.graphics.opengl 
+ kivy.graphics.opengl_utils 
+ kivy.graphics.scissor_instructions 
+ kivy.graphics.shader 
+ kivy.graphics.stencil_instructions 
+ kivy.graphics.svg 
+ kivy.graphics.tesselator 
+ kivy.graphics.texture 
+ kivy.graphics.transformation 
+ kivy.input 
+ kivy.input.factory 
+ kivy.input.motionevent 
+ kivy.input.postproc 
+ kivy.input.postproc.calibration 
+ kivy.input.postproc.dejitter 
+ kivy.input.postproc.doubletap 
+ kivy.input.postproc.ignorelist 
+ kivy.input.postproc.retaintouch 
+ kivy.input.postproc.tripletap 
+ kivy.input.provider 
+ kivy.input.providers 
+ kivy.input.providers.androidjoystick 
+ kivy.input.providers.hidinput 
+ kivy.input.providers.leapfinger 
+ kivy.input.providers.linuxwacom 
+ kivy.input.providers.mactouch 
+ kivy.input.providers.mouse 
+ kivy.input.providers.mtdev 
+ kivy.input.providers.probesysfs 
+ kivy.input.providers.tuio 
+ kivy.input.providers.wm_common 
+ kivy.input.providers.wm_pen 
+ kivy.input.providers.wm_touch 
+ kivy.input.recorder 
+ kivy.input.shape 
+ kivy.interactive 
+ kivy.lang 
+ kivy.lang.builder 
+ kivy.lang.parser 
+ kivy.lib 
+ kivy.lib.ddsfile 
+ kivy.lib.gstplayer 
+ kivy.lib.mtdev 
+ kivy.loader 
+ kivy.logger 
+ kivy.metrics 
+ kivy.modules 
+ kivy.modules.console 
+ kivy.modules.inspector 
+ kivy.modules.joycursor 
+ kivy.modules.keybinding 
+ kivy.modules.monitor 
+ kivy.modules.recorder 
+ kivy.modules.screen 
+ kivy.modules.touchring 
+ kivy.modules.webdebugger 
+ kivy.multistroke 
+ kivy.network 
+ kivy.network.urlrequest 
+ kivy.parser 
+ kivy.properties 
+ kivy.resources 
+ kivy.storage 
+ kivy.storage.dictstore 
+ kivy.storage.jsonstore 
+ kivy.storage.redisstore 
+ kivy.support 
+ kivy.tools 
+ kivy.tools.packaging 
+ kivy.tools.packaging.pyinstaller_hooks 
+ kivy.uix 
+ kivy.uix.accordion 
+ kivy.uix.actionbar 
+ kivy.uix.anchorlayout 
+ kivy.uix.behaviors 
+ kivy.uix.behaviors.button 
+ kivy.uix.behaviors.codenavigation 
+ kivy.uix.behaviors.compoundselection 
+ kivy.uix.behaviors.cover 
+ kivy.uix.behaviors.drag 
+ kivy.uix.behaviors.emacs 
+ kivy.uix.behaviors.focus 
+ kivy.uix.behaviors.knspace 
+ kivy.uix.behaviors.togglebutton 
+ kivy.uix.behaviors.touchripple 
+ kivy.uix.boxlayout 
+ kivy.uix.bubble 
+ kivy.uix.button
+ kivy.uix.camera 
+ kivy.uix.carousel 
+ kivy.uix.checkbox 
+ kivy.uix.codeinput 
+ kivy.uix.colorpicker 
+ kivy.uix.dropdown 
+ kivy.uix.effectwidget 
+ kivy.uix.filechooser 
+ kivy.uix.floatlayout 
+ kivy.uix.gesturesurface 
+ kivy.uix.gridlayout 
+ kivy.uix.image 
+ kivy.uix.label 
+ kivy.uix.layout 
+ kivy.uix.modalview 
+ kivy.uix.pagelayout 
+ kivy.uix.popup 
+ kivy.uix.progressbar 
+ kivy.uix.recycleboxlayout 
+ kivy.uix.recyclegridlayout 
+ kivy.uix.recyclelayout 
+ kivy.uix.recycleview 
+ kivy.uix.recycleview.datamodel 
+ kivy.uix.recycleview.layout 
+ kivy.uix.recycleview.views 
+ kivy.uix.relativelayout 
+ kivy.uix.rst 
+ kivy.uix.sandbox 
+ kivy.uix.scatter 
+ kivy.uix.scatterlayout 
+ kivy.uix.screenmanager 
+ kivy.uix.scrollview 
+ kivy.uix.settings 
+ kivy.uix.slider 
+ kivy.uix.spinner 
+ kivy.uix.splitter 
+ kivy.uix.stacklayout 
+ kivy.uix.stencilview 
+ kivy.uix.switch 
+ kivy.uix.tabbedpanel 
+ kivy.uix.textinput 
+ kivy.uix.togglebutton 
+ kivy.uix.treeview 
+ kivy.uix.video 
+ kivy.uix.videoplayer 
+ kivy.uix.vkeyboard 
+ kivy.uix.widget 
+ kivy.utils 
+ kivy.vector 
+ kivy.weakmethod 
+ kivy.weakproxy 
